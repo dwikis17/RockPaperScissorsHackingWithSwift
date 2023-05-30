ae43bce7cc29a8f6a4c0a0ed56799c42caf46c5d
Dwiki <dwikis17@gmail.com>
2023-05-30 19:46:36 +0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Dwiki Dwiki on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
